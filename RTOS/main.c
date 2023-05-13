60c487e4bac01e6dc7b238a01a07f03eb083f952
Kareem Azab <67200068+KareemAzab21@users.noreply.github.com>
2023-05-13 17:10:25 +0300

    refactor main.c

--- RTOS/main.c
+++ RTOS/main.c
@@ -34,14 +34,21 @@ void Drive_Motor_Forward_AUTO()
 {
 	 xSemaphoreTake( Mutex, 0 );//take el mutex
 	
-while(Limit_Pressed(LIMIT_1)==LOW && ButtonPressed(BUT_OBJECT)==HIGH)
-{
-		 MOTOR_A_FORWARD();
-}
+	while(Limit_Pressed(LIMIT_1)==LOW )
+	{
+			if(ButtonPressed(BUT_OBJECT)==LOW)
+				 {
+					 xSemaphoreGive(  Mutex );
+					 Object_Detect();
+					 return;
+				 }
+
+			 MOTOR_A_FORWARD();
+	}
 
 	MOTOR_A_STOP();
 	
-	  xSemaphoreGive(  Mutex ); //release el mutex
+	xSemaphoreGive(  Mutex ); //release el mutex
 	
 }
 void Drive_Motor_Forward_MANUAL()
@@ -51,7 +58,7 @@ void Drive_Motor_Forward_MANUAL()
 	 MOTOR_A_FORWARD();
 	
 	
-	  xSemaphoreGive(  Mutex ); //release el mutex
+	 xSemaphoreGive(  Mutex ); //release el mutex
 	
 }
 
@@ -72,7 +79,7 @@ void Drive_Motor_Backward_AUTO()
 
 void Drive_Motor_Backward_MANUAL()
 {
-		xSemaphoreTake( Mutex, 0 );//take el mutex
+	xSemaphoreTake( Mutex, 0 );//take el mutex
 	
 	 MOTOR_A_BACKWARD();
 	
@@ -101,8 +108,7 @@ void vSenderTask()
 	uint8 action;
 	while(1)
 	{
-		 if( ButtonPressed(BUTTON_A) == LOW ){
-       if(SwitchPressed()==HIGH)
+		   if(SwitchPressed()==HIGH)
        {
           //send to reciever to lock
 				 action=LOCK;
@@ -114,14 +120,16 @@ void vSenderTask()
           action=UNLOCK;
 				 (void)xQueueSendToBack( xQueue, &action, 0 );
        }
-					vTaskDelay(100 / portTICK_RATE_MS);
-			 	if(ButtonPressed(BUTTON_A)==HIGH && uxSemaphoreGetCount(xBinarySemaphore)==0)
+		 if( ButtonPressed(BUTTON_A) == LOW ){
+
+					vTaskDelay(150 / portTICK_RATE_MS);
+			 	if(ButtonPressed(BUTTON_A)==HIGH && uxSemaphoreGetCount(xBinarySemaphore)==1)
 				 {
 					 Drive_Motor_Forward_AUTO();
 				 }
  
 
-       while(ButtonPressed(BUTTON_A)==LOW && uxSemaphoreGetCount(xBinarySemaphore)==0 && Limit_Pressed(LIMIT_1)==LOW)
+       while(ButtonPressed(BUTTON_A)==LOW && uxSemaphoreGetCount(xBinarySemaphore)==1 && Limit_Pressed(LIMIT_1)==LOW && ButtonPressed(BUTTON_D)==HIGH)
        {
 				 //if(SwitchPressed()==LOW)
 					// break;
@@ -129,6 +137,10 @@ void vSenderTask()
 				 if(ButtonPressed(BUT_OBJECT)==LOW)
 				 {
 					 Object_Detect();
+					 break;
+				 }
+				 if(SwitchPressed()==HIGH)
+				 {
 					 break;
 				 }
 				 
@@ -144,26 +156,19 @@ void vSenderTask()
        }
 
 		 if( ButtonPressed(BUTTON_B) == LOW ){
-       if(SwitchPressed()==HIGH)
-       {
-          //send to reciever to lock
-				 action=LOCK;
-				 (void)xQueueSendToBack( xQueue, &action, 0 );
-				   
-       }
-       else
-       {
-          action=UNLOCK;
-				 (void)xQueueSendToBack( xQueue, &action, 0 );
-       }
-				 vTaskDelay(100 / portTICK_RATE_MS);
-				 if(ButtonPressed(BUTTON_B)==HIGH && uxSemaphoreGetCount(xBinarySemaphore)==0)
+
+				 vTaskDelay(150 / portTICK_RATE_MS);
+				 if(ButtonPressed(BUTTON_B)==HIGH && uxSemaphoreGetCount(xBinarySemaphore)==1)
 				 {
 					 Drive_Motor_Backward_AUTO();
 				 }       
 
-       while(ButtonPressed(BUTTON_B)==LOW && uxSemaphoreGetCount(xBinarySemaphore)==0 && Limit_Pressed(LIMIT_2)==LOW)
+       while(ButtonPressed(BUTTON_B)==LOW && uxSemaphoreGetCount(xBinarySemaphore)==1 && Limit_Pressed(LIMIT_2)==LOW && ButtonPressed(BUTTON_C)==HIGH)
        {
+				 if(SwitchPressed()==HIGH)
+				 {
+					 break;
+				 }
 
 					Drive_Motor_Backward_MANUAL();
 
@@ -175,25 +180,14 @@ void vSenderTask()
        }
 
 		 if( ButtonPressed(BUTTON_C) == LOW ){
-			 if(SwitchPressed()==HIGH)
-       {
-          //send to reciever to lock
-				 action=LOCK;
-				 (void)xQueueSendToBack( xQueue, &action, 0 );
-				   
-       }
-       else
-       {
-          action=UNLOCK;
-				 (void)xQueueSendToBack( xQueue, &action, 0 );
-       }
 
-					vTaskDelay(100 / portTICK_RATE_MS);
+
+					vTaskDelay(150 / portTICK_RATE_MS);
 				 if(ButtonPressed(BUTTON_C)==HIGH)
 				 {
 					 Drive_Motor_Forward_AUTO();
 				 }
-       while(ButtonPressed(BUTTON_C)==LOW && Limit_Pressed(LIMIT_1)==LOW)
+       while(ButtonPressed(BUTTON_C)==LOW && Limit_Pressed(LIMIT_1)==LOW && ButtonPressed(BUTTON_B)==HIGH)
        {
 					if(ButtonPressed(BUT_OBJECT)==LOW)
 					{
@@ -212,26 +206,13 @@ void vSenderTask()
        }
 
 		 if( ButtonPressed(BUTTON_D) == LOW ){
-       if(SwitchPressed()==HIGH)
-       {
-          //send to reciever to lock
-				 action=LOCK;
-				 (void)xQueueSendToBack( xQueue, &action, 0 );
-				   
-       }
-       else
-       {
-          action=UNLOCK;
-				 (void)xQueueSendToBack( xQueue, &action, 0 );
-       }
 
-
-				vTaskDelay(100 / portTICK_RATE_MS);
+				vTaskDelay(150 / portTICK_RATE_MS);
 				 if(ButtonPressed(BUTTON_D)==HIGH)
 				 {
 					 Drive_Motor_Backward_AUTO();
 				 }
-       while(ButtonPressed(BUTTON_D)==LOW && Limit_Pressed(LIMIT_2)==LOW)
+       while(ButtonPressed(BUTTON_D)==LOW && Limit_Pressed(LIMIT_2)==LOW && ButtonPressed(BUTTON_A)==HIGH)
        {
 
 
@@ -258,10 +239,12 @@ int main()
 	vSemaphoreCreateBinary( xBinarySemaphore );
 	Mutex=xSemaphoreCreateMutex();
 	
+	DIO_Init(PORTF,1,OUT,PULLDOWN);
+	DIO_Init(PORTF,2,OUT,PULLDOWN);
 	 MOTOR_A_INIT();
    ButtonInit();
    SwitchInit();
-	Limit_Init();
+	 Limit_Init();
 
 	
 	
